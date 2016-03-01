723c4007c1843c66d3b907aeff380fb9b1b20ebb
Megrez <lujiajing1126@gmail.com>
2016-03-01 21:17:55 +0800

    Add JNDI & War

--- src/main/java/coder/dog/chosen/Application.java
+++ src/main/java/coder/dog/chosen/Application.java
@@ -5,17 +5,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by megrez on 16/2/26.
  */
 @SpringBootApplication
 @EnableConfigurationProperties(MemcachedConfiguration.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.out.println("Initializing the WebXml!");
+        return application.sources(Application.class);
     }
 }
---
+++ src/main/java/coder/dog/chosen/util/DataSourceJNDIConf.java
@@ -0,0 +1,54 @@
+package coder.dog.chosen.util;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * Created by megrez on 16/3/1.
+ */
+@Configuration
+public class DataSourceJNDIConf {
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+        return new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(Tomcat tomcat) {
+                tomcat.enableNaming();
+                return super.getTomcatEmbeddedServletContainer(tomcat);
+            }
+
+            @Override
+            protected void postProcessContext(Context context) {
+                super.postProcessContext(context);
+                ContextResource resource = new ContextResource();
+                resource.setName("jdbc/myDataSource");
+                resource.setType(DataSource.class.getName());
+                resource.setProperty("driverClassName", "org.postgresql.Driver");
+                resource.setProperty("url", "jdbc:postgresql://localhost/chosen");
+                resource.setProperty("user","megrez");
+                resource.setProperty("password","123456789");
+                context.getNamingResources().addResource(resource);
+            }
+        };
+    }
+
+    @Bean(destroyMethod="")
+    public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+        bean.setJndiName("java:comp/env/jdbc/myDataSource");
+        bean.setProxyInterface(DataSource.class);
+        bean.setLookupOnStartup(false);
+        bean.afterPropertiesSet();
+        return (DataSource)bean.getObject();
+    }
+}
